94f53487bb71f5569465b57a371f0353ed280449
VladyslavTrach <trach.vladyslav28@gmail.com>
2025-05-12 17:30:50 +0300

    change architecture

---
+++ config.py
@@ -0,0 +1,7 @@
+# Параметри автозаповнення та режим роботи форми
+SHOULD_FILL_DATA = False  # True — вводити дані, False — лише перевіряти
+LAST_NAME = "Трач"
+FIRST_NAME = "Владислав"
+SECOND_NAME = "Ігорович"
+EMAIL = "vlad09121978@gmail.com"
+PHONE = "673480565"
